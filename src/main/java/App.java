4a32787b59ab42ea78ddab1a5d30eafcd6a89ca0
Evan Johnson and Esti Shay <esti@>
2017-08-07 11:13:59 -0700

    add form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,28 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> data = new HashMap<String, Object>();
+            return new ModelAndView(data, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/change", (request, response) -> {
+            Map<String, Object> data = new HashMap<String, Object>();
+            String input = request.queryParams("amount");
+            Float cash = Float.parseFloat(input);
+            ChangeMachine changeMachine = new ChangeMachine();
+            String output = changeMachine.makeChange(cash);
+            data.put("amount", output);
+            return new ModelAndView(data, "change.hbs");
+
+        }, new HandlebarsTemplateEngine());
 
     }
 }
